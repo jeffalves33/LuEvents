79a1336e5064610fd2bfafd9e55353bd4adc282c
Jeferson <luiz_cleidi@hotmail.com>
2024-10-23 19:48:05 -0300

    feat: funcionalidade da tela inicial

--- routes/tickets.js
+++ routes/tickets.js
@@ -5,27 +5,15 @@ const path = require('path');
 
 // Rota para selecionar ingressos
 router.get('/select-tickets', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../public/select-tickets.html'));
+    res.render('tickets/select-tickets');
 });
 
-router.post('/select-seats', (req, res) => {
-    const tickets = req.body.tickets;
-    res.sendFile(path.resolve(__dirname + '/../public/select-seats.html'));
+router.get('/select-seats', (req, res) => {
+    res.render('tickets/select-seats');
 });
 
-router.post('/enter-cpf', (req, res) => {
-    const seats = req.body['selected-seats'];
-    res.sendFile(path.resolve(__dirname + '/../public/enter-cpf.html'));
-});
-
-router.post('/enter-details', (req, res) => {
-    const cpf = req.body.cpf;
-    res.sendFile(path.resolve(__dirname + '/../public/enter-details.html'));
-});
-
-router.post('/payment', (req, res) => {
-    const details = req.body;
-    res.sendFile(path.resolve(__dirname + '/../public/payment.html'));
+router.get('/change-seats', (req, res) => {
+    res.render('tickets/change-seats');
 });
 
 module.exports = router;
