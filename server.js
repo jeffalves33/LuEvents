52e64bd88062d3ef612c042a40d1389f4a96f9f2
Jeferson <luiz_cleidi@hotmail.com>
2024-11-05 19:47:09 -0300

    fix: 2 Vercel file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 
 const indexRouter = require('./routes/index');
